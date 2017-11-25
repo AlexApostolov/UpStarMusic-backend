e43dbb73b6fbd84dcab69483b0a2fa614a513309
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-11-24 21:17:07 -0500

    Wire up Album & Artist models. Create FindArtist query.

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,13 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date, // Reference to the global date object inside JavaScript
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number // Store currency as raw number--no dollar sign or commas--to easily convert currency/format
+});
+
+module.exports = AlbumSchema;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,19 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number, // A DOB as a String would need no future updates
+  yearsActive: Number, // A start date for career would need no future updates
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number, // Label should have currency name in it, not assume USD
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('Artist', ArtistSchema);
+module.exports = Artist;
